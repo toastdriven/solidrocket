7e1a56a6f27cd4b907129d5ad88761287c244285
Daniel Lindsley <daniel@toastdriven.com>
2011-01-25 04:00:54 -0600

    Converted tests to telescope.

--- tests/database.lua
+++ tests/database.lua
@@ -2,48 +2,62 @@ require("solidrocket/database")
 require("solidrocket/utils/path")
 
 
-function test_database()
-  -- Make a test directory we can crap all over in.
-  local database_root = '/tmp/solidrockettest'
-  local path = Path()
-  -- Pre-wipe in case of previous failures.
-  path:rmtree(database_root)
-  -- Rebuild.
-  path:makedirs(database_root)
+context("solidrocket", function()
+  before(function()
+    -- Make a test directory we can crap all over in.
+    database_root = '/tmp/solidrockettest'
+    path = Path()
+    -- Pre-wipe in case of previous failures.
+    path:rmtree(database_root)
+    -- Rebuild.
+    path:makedirs(database_root)
+  end)
   
-  -- Make sure the path doesn't exist.
-  assert(path:exists(path:join({database_root, 'test1'})) == false, "Path to 'test1' exists.")
+  after(function()
+    -- Clean up.
+    path:rmtree(database_root)
+  end)
   
-  local db1 = Database(database_root, 'test1')
-  assert(path:exists(path:join({database_root, 'test1'})) == true, "Path to 'test1' was not created.")
-  
-  -- Reopening an existing database shouldn't cause an error.
-  local db2 = Database(database_root, 'test1')
-  assert(path:exists(path:join({database_root, 'test1'})) == true, "Path to 'test1' can't be loaded.")
-  
-  -- Another database alongside shouldn't cause an error.
-  assert(path:exists(path:join({database_root, 'test2'})) == false, "Path to 'test2' exists.")
-  local db3 = Database(database_root, 'test2')
-  assert(path:exists(path:join({database_root, 'test2'})) == true, "Path to 'test2' was not created.")
-  
-  -- Test database deletion.
-  local db3 = Database(database_root, 'test2')
-  assert(path:exists(path:join({database_root, 'test2'})) == true, "Path to 'test2' does not exist.")
-  assert(db3:delete() == true, "Calling delete on database failed.")
-  assert(path:exists(path:join({database_root, 'test2'})) == false, "Path to 'test2' was not deleted.")
-  assert(db3:delete() == true, "Calling delete on database a second time failed.")
-  
-  -- Test database name modifications.
-  local db4 = Database(database_root, 'test1')
-  assert(db4:clean_database_name() == 'test1', "Incorrect cleaning of name 'test1'.")
-  db4.database_name = 'test_some-special.chars'
-  assert(db4:clean_database_name() == 'test_some-special.chars', "Incorrect cleaning of name 'test_some-special.chars'.")
-  db4.database_name = 'test/../some/.../invalid#char$'
-  assert(db4:clean_database_name() == 'test..some...invalidchar', "Incorrect cleaning of name 'test/../some/.../invalid#char$'.")
-  
-  -- Clean up.
-  path:rmtree(database_root)
-end
+  context("database", function()
+    test("opening", function()
+      -- Make sure the path doesn't exist.
+      assert_equal(path:exists(path:join({database_root, 'test1'})), false)
 
+      local db1 = Database(database_root, 'test1')
+      assert_equal(path:exists(path:join({database_root, 'test1'})), true)
 
-test_database()
+      -- Reopening an existing database shouldn't cause an error.
+      local db2 = Database(database_root, 'test1')
+      assert_equal(path:exists(path:join({database_root, 'test1'})), true)
+
+      -- Another database alongside shouldn't cause an error.
+      assert_equal(path:exists(path:join({database_root, 'test2'})), false)
+      
+      local db3 = Database(database_root, 'test2')
+      assert_equal(path:exists(path:join({database_root, 'test2'})), true)
+    end)
+    
+    test("deleting", function()
+      -- Test database deletion.
+      local db3 = Database(database_root, 'test2')
+      assert_equal(path:exists(path:join({database_root, 'test2'})), true)
+      assert_equal(db3:delete(), true)
+      
+      -- Calling delete again shouldn't fail.
+      assert_equal(path:exists(path:join({database_root, 'test2'})), false)
+      assert_equal(db3:delete(), true)
+    end)
+    
+    test("naming", function()
+      -- Test database name modifications.
+      local db4 = Database(database_root, 'test1')
+      assert_equal(db4:clean_database_name(), 'test1')
+      
+      db4.database_name = 'test_some-special.chars'
+      assert_equal(db4:clean_database_name(), 'test_some-special.chars')
+      
+      db4.database_name = 'test/../some/.../invalid#char$'
+      assert_equal(db4:clean_database_name(), 'test..some...invalidchar')
+    end)
+  end)
+end)
